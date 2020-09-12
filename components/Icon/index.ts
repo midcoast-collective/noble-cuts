7bda266fd5ff26849e728b5d884d2ca1ac5ffcff
Nate Arnold <hello@natearnold.me>
2020-09-12 11:32:31 -0500

    Add logo

--- components/Icon/index.ts
+++ components/Icon/index.ts
@@ -1,9 +1,5 @@
-import { Branding } from "./Branding";
-import { Delivery } from "./Delivery";
 import { Facebook } from "./Facebook";
 import { Logo } from "./Logo";
-import { Mask } from "./Mask";
-import { Takeout } from "./Takeout";
 import { Twitter } from "./Twitter";
 
-export { Branding, Delivery, Facebook, Logo, Mask, Takeout, Twitter };
+export { Facebook, Logo, Twitter };
