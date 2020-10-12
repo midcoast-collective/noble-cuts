3c5f2e10b30b9da898406fc25e10823c423a65f4
Nate Arnold <hello@natearnold.me>
2020-10-12 08:02:20 -0500

    Convert total to number

--- functions/getCheckoutLink.js
+++ functions/getCheckoutLink.js
@@ -1,17 +1,17 @@
 const squareConnect = require("square-connect");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
-const squareClient = squareConnect.ApiClient.instance
+const squareClient = squareConnect.ApiClient.instance;
 const oauth2 = squareClient.authentications["oauth2"];
 
 oauth2.accessToken = process.env.SANDBOX_ACCESS_TOKEN;
 // oauth2.accessToken = process.env.ACCESS_TOKEN;
-squareClient.basePath = "https://connect.squareupsandbox.com"
+squareClient.basePath = "https://connect.squareupsandbox.com";
 // squareClient.basePath = "https://connect.squareup.com"
 
 exports.handler = async function (event, context, callback) {
   const data = JSON.parse(event.body);
-  const { cartTotal, nonce, buyerVerificationToken } = data
+  const { cartTotal, nonce, buyerVerificationToken } = data;
 
   const idempotencyKey = uuidv4();
 
@@ -22,22 +22,21 @@ exports.handler = async function (event, context, callback) {
     source_id: nonce,
     verification_token: buyerVerificationToken,
     amount_money: {
-      amount: cartTotal,
-      currency: 'USD'
+      amount: parseInt(cartTotal, 10),
+      currency: "USD",
     },
-    idempotency_key: idempotencyKey
+    idempotency_key: idempotencyKey,
   };
 
   try {
     const response = await paymentsAPI.createPayment(requestBody);
     const json = JSON.stringify(response);
-    console.log('API called successfully. Returned data: ' + data);
+    console.log("API called successfully. Returned data: " + data);
 
     callback(null, {
       statusCode: 200,
-      body: json
+      body: json,
     });
-
   } catch (error) {
     console.error(error);
     callback(err);
