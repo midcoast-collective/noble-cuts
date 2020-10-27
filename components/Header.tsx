a8f0934dbd93d2e75df5768de42d0ea6f0d6e28d
Nate Arnold <hello@natearnold.me>
2020-10-27 09:32:10 -0500

    Add contact links

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,50 +7,63 @@ type HeaderProps = {};
 
 const Header = ({}: Readonly<HeaderProps>) => {
   return (
-    <header>
-      <nav className="desktop-only">
-        <Link href="/">
-          <a>HOME</a>
-        </Link>
-        <Link href="/about/">
-          <a>ABOUT</a>
-        </Link>
-        <Link href="/">
-          <a className="logo">
-            <Icon.HeaderLogo />
-          </a>
-        </Link>
-        <Link href="/#products">
-          <a>SHOP</a>
-        </Link>
-        <Link href="/cart/">
-          <a>CART</a>
-        </Link>
-      </nav>
+    <>
+      <div className="hairline">
+        <div className="wrap">
+          <div className="hairline-links">
+            <span>Contact: </span>
+            <a href="tel:316-708-2134">(316) 708-2134</a>
+            <a href="mailto:ashley@noblecutsmeat.com">
+              ashley@noblecutsmeat.com
+            </a>
+          </div>
+        </div>
+      </div>
+      <header>
+        <nav className="desktop-only">
+          <Link href="/">
+            <a>HOME</a>
+          </Link>
+          <Link href="/about/">
+            <a>ABOUT</a>
+          </Link>
+          <Link href="/">
+            <a className="logo">
+              <Icon.HeaderLogo />
+            </a>
+          </Link>
+          <Link href="/#products">
+            <a>SHOP</a>
+          </Link>
+          <Link href="/cart/">
+            <a>CART</a>
+          </Link>
+        </nav>
 
-      <nav className="mobile-only">
-        <Link href="/">
-          <a className="logo">
-            <Icon.HeaderLogo />
-          </a>
-        </Link>
-      </nav>
+        <nav className="mobile-only">
+          <Link href="/">
+            <a className="logo">
+              <Icon.HeaderLogo />
+            </a>
+          </Link>
+        </nav>
 
-      <nav className="mobile-only">
-        <Link href="/">
-          <a>HOME</a>
-        </Link>
-        <Link href="/about/">
-          <a>ABOUT</a>
-        </Link>
-        <Link href="/#products">
-          <a>SHOP</a>
-        </Link>
-        <Link href="/cart/">
-          <a>CART</a>
-        </Link>
-      </nav>
-    </header>
+        <nav className="mobile-only">
+          <Link href="/">
+            <a>HOME</a>
+          </Link>
+          <Link href="/about/">
+            <a>ABOUT</a>
+          </Link>
+          <Link href="/#products">
+            <a>SHOP</a>
+          </Link>
+          <Link href="/cart/">
+            <a>CART</a>
+          </Link>
+        </nav>
+      </header>
+    </>
   );
 };
 
